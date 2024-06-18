8bc8a914dd0353af157b5057a201522d34476acd
Hugo22102003 <165829737+Hugo22102003@users.noreply.github.com>
2024-06-18 15:17:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ migrate = Migrate()
 migrate.init_app(app, db)
 
 #* Configuracion de flask-wtf
-app.config['SECRET_KEY'] = 'llave_secreta'
+app.config['SECRET_KEY'] = 'Chupame las bolas perra'
 
 #* Configuracion del login de flask
 login_manager = LoginManager()
