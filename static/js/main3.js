7af07b1dbe6029ec0f426a48530c1467f83ab1ce
hugo22102003 <hugon221003@gmail.com>
2024-06-20 15:47:53 -0400

    Intentemos otra vez

--- static/js/main3.js
+++ static/js/main3.js
@@ -60,7 +60,6 @@ formulario.addEventListener('submit', (e) => {
 
     e.preventDefault();
     if (campos.nombre && campos.cedula && campos.telefono && campos.tipo ){
-        formulario.reset();
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
         setTimeout(() =>{
             document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
